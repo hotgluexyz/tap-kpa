8256376cb7f3c2b6bf50a34c1f8a63bebcd93821
raheelhotglue <raheel@hotglue.io>
2026-06-08 22:54:09 +0500

    HGI-10345: : type decimals as number and subreport as integer (match hgi-6323) (#14)

--- tap_kpa/client.py
+++ tap_kpa/client.py
@@ -98,7 +98,7 @@ class KpaStream(RESTStream):
             return th.ArrayType(th.StringType)
         if field.get("type") == "datetime":
             return th.DateTimeType
-        if field.get("type") == "counter":
+        if field.get("type") in ["counter", "subreport"]:
             return th.IntegerType
         if field.get("type") in ["sketch", "attachments"]:
             return th.ArrayType(th.CustomType({"type": ["object", "string"]}))
